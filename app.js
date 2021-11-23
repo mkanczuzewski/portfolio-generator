818196860b5973984a533f903e9ccde8dbf94159
mkanczuzewski <mkanczuzewski@gmail.com>
2021-11-23 11:37:54 -0500

    generate html and modularize code

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js'); //run the generate page function from the page-template.js file
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
